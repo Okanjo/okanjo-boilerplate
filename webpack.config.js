3edf75c1f1cbded4752747cb764314b0ceca5c7c
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2022-11-02 10:35:12 -0500

    - gulpfile.js: Removed deprecated reference gulpfile.js - webpack.config.js: Updated support for multiple builds - .gitignore: Remove dist files from commits - index.j2: Changed vendor builds to include bundle name (support multiple vendor builds)

--- webpack.config.js
+++ webpack.config.js
@@ -3,94 +3,92 @@
 const Path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.env === "production";
 
-module.exports = {
+module.exports = [
 
-    // Environment
-    mode: isProduction ? "production" : 'development',
+    //region Main app
+    {
+        // Environment
+        mode: isProduction ? "production" : 'development',
 
-    // devtool: "eval", // fast!
-    // devtool: "eval-source-map", // average, but good!
-    // devtool: "source-map", // dvd quality!
-    devtool: isProduction ? "source-map" : "eval-source-map",
+        // devtool: "eval", // fast!
+        // devtool: "eval-source-map", // average, but good!
+        // devtool: "source-map", // dvd quality!
+        devtool: isProduction ? "source-map" : "eval-source-map",
 
-    // Entry points
-    entry: {
-        main: [
-            // App
-            './ui/src/index.js',
-            './ui/styles/main.scss',
-        ],
-        // vendor: [
-        //     'node_modules/whatwg-fetch/dist/fetch.umd.js'
-        //     'node_modules/socket.io-client/socket.io.js'
-        //     'node_modules/es6-promise/dist/es6-promise.min.js'
-        //     'node_modules/html5shiv/dist/html5shiv.min.js'
-        // ]
-    },
+        // Entry points
+        entry: {
+            main: [
+                // App
+                './ui/src/index.js',
+                './ui/styles/main.scss',
+            ],
+        },
 
-    // Convert JSX and JS to ES5, and SCSS too
-    module: {
-        rules: [
-            // JSX
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-
-            // SCSS
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    // "style-loader",
-                    MiniCssExtractPlugin.loader,
-
-                    // Translates CSS into CommonJS
-                    { loader: "css-loader", options: { sourceMap: true } },
-
-                    // Compiles Sass to CSS
-                    { loader: "sass-loader", options: { sourceMap: true } },
-                ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-
-    // Plugin to drop css in to static files
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css-bundle-[name].css',
-            chunkFilename: 'css-bundle-[id].css'
-        })
-    ],
-
-    // Separate node_modules vendor libs to separate bundle file
-    optimization: {
-        splitChunks: {
-            chunks: "all",
-            maxInitialRequests: Infinity,
-            minSize: 0,
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendor",
+        // Convert JSX and JS to ES5, and SCSS too
+        module: {
+            rules: [
+                // JSX
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
                 },
-                cssVendor: {
-                    test: /[\\/]vendor.scss]/,
-                    name: "vendor",
+
+                // SCSS
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        // "style-loader",
+                        MiniCssExtractPlugin.loader,
+
+                        // Translates CSS into CommonJS
+                        {loader: "css-loader", options: {sourceMap: true}},
+
+                        // Compiles Sass to CSS
+                        {loader: "sass-loader", options: {sourceMap: true}},
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx']
+        },
+
+        // Plugin to drop css in to static files
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'css-bundle-[name].css',
+                chunkFilename: 'css-bundle-[id].css'
+            })
+        ],
+
+        // Separate node_modules vendor libs to separate bundle file
+        optimization: {
+            splitChunks: {
+                chunks: "all",
+                maxInitialRequests: Infinity,
+                minSize: 0,
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "main-vendor",
+                    },
+                    cssVendor: {
+                        test: /[\\/]vendor.scss]/,
+                        name: "vendor",
+                    }
                 }
             }
-        }
-    },
+        },
 
-    // Output
-    output: {
-        path: Path.resolve(__dirname, 'ui/static/dist'),
-        filename: 'app-bundle-[name].js',
-    },
-};
+        // Output
+        output: {
+            path: Path.resolve(__dirname, 'ui/static/dist'),
+            filename: 'app-bundle-[name].js',
+        },
+    }
+    //endregion
+];
